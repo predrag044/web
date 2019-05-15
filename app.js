35bfe92160a77e42ba09cc0ccfc37a2722bbfeed
Predrag Obracevic <predrag.obracevic@gmail.com>
2019-05-15 16:21:50 +0200

    test build

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('- Not Found -');
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
